f59840c208453a52b68702e51d7adbe56669249c
daniielperez <daniel.perezluna94@gmail.com>
2016-10-06 17:04:13 -0500

    commit

---
+++ angular/app/model/tramite/Tramite.js
@@ -0,0 +1,15 @@
+"use strict";
+var Tramite = (function () {
+    function Tramite(id, nombre, valor, redondeo, unidad, afectacion, modulo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.valor = valor;
+        this.redondeo = redondeo;
+        this.unidad = unidad;
+        this.afectacion = afectacion;
+        this.modulo = modulo;
+    }
+    return Tramite;
+}());
+exports.Tramite = Tramite;
+//# sourceMappingURL=Tramite.js.map
---
+++ angular/app/model/tramite/tramite.ts
@@ -0,0 +1,11 @@
+export class Tramite{
+	constructor(
+		public id:number,
+		public nombre:string,
+		public valor:number,
+		public redondeo:string,
+		public unidad:string,
+		public afectacion:boolean,
+		public modulo:number
+	){}
+}
