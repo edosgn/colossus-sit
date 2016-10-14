ac256fbc89b2a05f7434c63b5c35a223c183cbf6
davidperezluna <david.perez.udenar@gmail.com>
2016-10-14 17:36:23 -0500

    subir carpetas

--- angular/app/components/subirCarpeta/index.subirCarpeta.component.js
+++ angular/app/components/subirCarpeta/index.subirCarpeta.component.js
@@ -11,19 +11,23 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 // Importar el núcleo de Angular
 var ciudadanoVehiculo_service_1 = require("../../services/ciudadanoVehiculo/ciudadanoVehiculo.service");
 var vehiculo_service_1 = require("../../services/vehiculo/vehiculo.service");
+var ciudadano_service_1 = require("../../services/ciudadano/ciudadano.service");
 var login_service_1 = require("../../services/login.service");
 var core_1 = require('@angular/core');
 var router_1 = require("@angular/router");
 var new_vehiculo_component_1 = require('../../components/vehiculo/new.vehiculo.component');
 var new_ciudadano_component_1 = require('../../components/ciudadano/new.ciudadano.component');
+var CiudadanoVehiculo_1 = require('../../model/CiudadanoVehiculo/CiudadanoVehiculo');
 // Decorador component, indicamos en que etiqueta se va a cargar la 
 var IndexSubirCarpetaComponent = (function () {
-    function IndexSubirCarpetaComponent(_VehiculoService, _CiudadanoVehiculoService, _loginService, _route, _router) {
+    function IndexSubirCarpetaComponent(_VehiculoService, _CiudadanoService, _CiudadanoVehiculoService, _loginService, _route, _router) {
         this._VehiculoService = _VehiculoService;
+        this._CiudadanoService = _CiudadanoService;
         this._CiudadanoVehiculoService = _CiudadanoVehiculoService;
         this._loginService = _loginService;
         this._route = _route;
         this._router = _router;
+        this.ciudadanoVehiculo = new CiudadanoVehiculo_1.CiudadanoVehiculo(null, null, null, "", "", "", "");
     }
     IndexSubirCarpetaComponent.prototype.ngOnInit = function () {
         var _this = this;
@@ -48,6 +52,7 @@ var IndexSubirCarpetaComponent = (function () {
                 _this.crear = true;
                 _this.claseSpan = "glyphicon glyphicon-remove form-control-feedback";
                 _this.clase = "form-group has-error has-feedback";
+                _this.activar = false;
             }
             else {
                 _this.claseSpan = "glyphicon glyphicon-ok form-control-feedback";
@@ -91,14 +96,73 @@ var IndexSubirCarpetaComponent = (function () {
         this.placa.placa = event;
         this.onKey("");
     };
+    IndexSubirCarpetaComponent.prototype.onKeyCiudadano = function (event) {
+        var _this = this;
+        var identificacion = {
+            'numeroIdentificacion': event,
+        };
+        var token = this._loginService.getToken();
+        this._CiudadanoService.showCiudadanoCedula(token, identificacion).subscribe(function (response) {
+            _this.ciudadano = response.data;
+            var status = response.status;
+            for (var i = _this.ciudadanosVehiculo.length - 1; i >= 0; i--) {
+                if (_this.ciudadanosVehiculo[i].ciudadano.numeroIdentificacion == event) {
+                    var existe = true;
+                }
+            }
+            if (existe) {
+                alert("existe una relacion con el ciudadano");
+            }
+            else {
+                if (status == 'error') {
+                    _this.validateCedula = false;
+                    _this.claseSpanCedula = "glyphicon glyphicon-remove form-control-feedback";
+                    _this.calseCedula = "form-group has-error has-feedback";
+                }
+                else {
+                    _this.validateCedula = true;
+                    _this.claseSpanCedula = "glyphicon glyphicon-ok form-control-feedback";
+                    _this.calseCedula = "form-group has-success has-feedback";
+                    _this.msgCiudadano = response.msj;
+                }
+            }
+        }, function (error) {
+            _this.errorMessage = error;
+            if (_this.errorMessage != null) {
+                console.log(_this.errorMessage);
+                alert("Error en la petición");
+            }
+        });
+    };
+    IndexSubirCarpetaComponent.prototype.VehiculoCiudadano = function () {
+        var _this = this;
+        this.ciudadanoVehiculo.ciudadanoId = this.ciudadano.numeroIdentificacion;
+        this.ciudadanoVehiculo.vehiculoId = this.vehiculo.placa;
+        var token = this._loginService.getToken();
+        this._CiudadanoVehiculoService.register(this.ciudadanoVehiculo, token).subscribe(function (response) {
+            _this.respuesta = response;
+            if (_this.respuesta.status == 'success') {
+                _this.validateCedula = false;
+                _this.onKey("");
+            }
+            console.log(_this.respuesta);
+            (function (error) {
+                _this.errorMessage = error;
+                if (_this.errorMessage != null) {
+                    console.log(_this.errorMessage);
+                    alert("Error en la petición");
+                }
+            });
+        });
+    };
     IndexSubirCarpetaComponent = __decorate([
         core_1.Component({
             selector: 'default',
             templateUrl: 'app/view/subirCarpeta/index.component.html',
             directives: [router_1.ROUTER_DIRECTIVES, new_vehiculo_component_1.NewVehiculoComponent, new_ciudadano_component_1.NewCiudadanoComponent],
-            providers: [login_service_1.LoginService, vehiculo_service_1.VehiculoService, ciudadanoVehiculo_service_1.CiudadanoVehiculoService]
+            providers: [login_service_1.LoginService, vehiculo_service_1.VehiculoService, ciudadanoVehiculo_service_1.CiudadanoVehiculoService, ciudadano_service_1.CiudadanoService]
         }), 
-        __metadata('design:paramtypes', [vehiculo_service_1.VehiculoService, ciudadanoVehiculo_service_1.CiudadanoVehiculoService, login_service_1.LoginService, router_1.ActivatedRoute, router_1.Router])
+        __metadata('design:paramtypes', [vehiculo_service_1.VehiculoService, ciudadano_service_1.CiudadanoService, ciudadanoVehiculo_service_1.CiudadanoVehiculoService, login_service_1.LoginService, router_1.ActivatedRoute, router_1.Router])
     ], IndexSubirCarpetaComponent);
     return IndexSubirCarpetaComponent;
 }());
--- angular/app/components/subirCarpeta/index.subirCarpeta.component.ts
+++ angular/app/components/subirCarpeta/index.subirCarpeta.component.ts
@@ -1,12 +1,14 @@
 // Importar el núcleo de Angular
 import {CiudadanoVehiculoService} from "../../services/ciudadanoVehiculo/ciudadanoVehiculo.service";
 import {VehiculoService} from "../../services/vehiculo/vehiculo.service";
+import {CiudadanoService} from "../../services/ciudadano/ciudadano.service";
 import {LoginService} from "../../services/login.service";
 import {Component, OnInit} from '@angular/core';
 import { ROUTER_DIRECTIVES, Router, ActivatedRoute } from "@angular/router";
 import {Tramite} from '../../model/tramite/Tramite';
 import {NewVehiculoComponent} from '../../components/vehiculo/new.vehiculo.component';
 import {NewCiudadanoComponent} from '../../components/ciudadano/new.ciudadano.component';
+import {CiudadanoVehiculo} from '../../model/CiudadanoVehiculo/CiudadanoVehiculo';
  
 // Decorador component, indicamos en que etiqueta se va a cargar la 
 
@@ -14,17 +16,18 @@ import {NewCiudadanoComponent} from '../../components/ciudadano/new.ciudadano.co
     selector: 'default',
     templateUrl: 'app/view/subirCarpeta/index.component.html',
     directives: [ROUTER_DIRECTIVES, NewVehiculoComponent,NewCiudadanoComponent],
-    providers: [LoginService,VehiculoService,CiudadanoVehiculoService]
+    providers: [LoginService,VehiculoService,CiudadanoVehiculoService,CiudadanoService]
 })
  
 // Clase del componente donde irán los datos y funcionalidades
 export class IndexSubirCarpetaComponent implements OnInit{ 
-	public errorMessage;
+	public errorMessage; 
 	public tramiteId;
 	public respuesta;
 	public activar;
 	public vehiculo;
 	public validate;
+	public ciudadanoVehiculo: CiudadanoVehiculo;
 	public clase;
 	public msg;
 	public claseSpan;
@@ -37,17 +40,20 @@ export class IndexSubirCarpetaComponent implements OnInit{
 	public crear;
 	public placa;
 	public resive;
+	public identificacion;
+	public ciudadano;
 
 
 
 	constructor(
 		private _VehiculoService: VehiculoService,
+		private _CiudadanoService: CiudadanoService,
 		private _CiudadanoVehiculoService: CiudadanoVehiculoService,
 		private _loginService: LoginService,
 		private _route: ActivatedRoute,
 		private _router: Router
 		
-		){}
+		){this.ciudadanoVehiculo = new CiudadanoVehiculo(null, null,null,"","","","");}
 
 
 	ngOnInit(){	
@@ -78,6 +84,7 @@ export class IndexSubirCarpetaComponent implements OnInit{
 						this.crear=true;
 						this.claseSpan ="glyphicon glyphicon-remove form-control-feedback";
 						this.clase = "form-group has-error has-feedback";
+						this.activar =false;
 					}else{
 							this.claseSpan ="glyphicon glyphicon-ok form-control-feedback";
 							this.clase = "form-group has-success has-feedback";
@@ -132,6 +139,76 @@ export class IndexSubirCarpetaComponent implements OnInit{
   vheiculoCreado(event:any) {
   	this.placa.placa=event
     this.onKey("");
+  }
+
+  onKeyCiudadano(event:any){
+  	let identificacion = {
+ 		'numeroIdentificacion' : event,
+ 	};
+  	let token = this._loginService.getToken();
+  	this._CiudadanoService.showCiudadanoCedula(token,identificacion).subscribe(
+				response => {
+					this.ciudadano = response.data;
+					let status = response.status;
+
+					for (var i = this.ciudadanosVehiculo.length - 1; i >= 0; i--) {
+						if(this.ciudadanosVehiculo[i].ciudadano.numeroIdentificacion == event) {
+							let existe = true;
+						}
+					}
+					if(existe){
+						alert ("existe una relacion con el ciudadano");
+					}else{
+							if(status == 'error') {
+						this.validateCedula=false;
+						this.claseSpanCedula ="glyphicon glyphicon-remove form-control-feedback";
+						this.calseCedula = "form-group has-error has-feedback";
+						}else{
+							this.validateCedula=true;
+							this.claseSpanCedula ="glyphicon glyphicon-ok form-control-feedback";
+							this.calseCedula = "form-group has-success has-feedback";
+				            this.msgCiudadano = response.msj;
+						}
+					}
+
+					
+				}, 
+				error => {
+					this.errorMessage = <any>error;
+
+					if(this.errorMessage != null){
+						console.log(this.errorMessage);
+						alert("Error en la petición");
+					}
+				}
+			);
+  }
+
+  VehiculoCiudadano(){
+
+  	this.ciudadanoVehiculo.ciudadanoId=this.ciudadano.numeroIdentificacion;
+  	this.ciudadanoVehiculo.vehiculoId=this.vehiculo.placa;
+
+  	let token = this._loginService.getToken();
+		this._CiudadanoVehiculoService.register(this.ciudadanoVehiculo,token).subscribe(
+			response => {
+				this.respuesta = response;
+				if(this.respuesta.status=='success') {
+					this.validateCedula=false;
+					this.onKey("");
+
+				}
+				console.log(this.respuesta);
+			error => {
+					this.errorMessage = <any>error;
+
+					if(this.errorMessage != null){
+						console.log(this.errorMessage);
+						alert("Error en la petición");
+					}
+				}
+
+		});
   }
  
 }
