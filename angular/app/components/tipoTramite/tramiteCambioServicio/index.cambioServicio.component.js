ca89f006eeec2aaad0d7d89bf76ccd92d1bd212f
davidperezluna <david.perez.udenar@gmail.com>
2016-10-25 11:35:18 -0500

    tramite cambio servicio

---
+++ angular/app/components/tipoTramite/tramiteCambioServicio/index.cambioServicio.component.js
@@ -0,0 +1,142 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+// Importar el núcleo de Angular
+var login_service_1 = require("../../../services/login.service");
+var core_1 = require('@angular/core');
+var router_1 = require("@angular/router");
+var servicio_service_1 = require("../../../services/servicio/servicio.service");
+var vehiculo_1 = require("../../../model/vehiculo/vehiculo");
+var tramiteEspecifico_service_1 = require("../../../services/tramiteEspecifico/tramiteEspecifico.service");
+var TramiteEspecifico_1 = require('../../../model/tramiteEspecifico/TramiteEspecifico');
+var vehiculo_service_1 = require("../../../services/vehiculo/vehiculo.service");
+var variante_service_1 = require("../../../services/variante/variante.service");
+var caso_service_1 = require("../../../services/caso/caso.service");
+// Decorador component, indicamos en que etiqueta se va a cargar la 
+var NewTramiteCambioServicioComponent = (function () {
+    function NewTramiteCambioServicioComponent(_TramiteEspecificoService, _VarianteService, _CasoService, _VehiculoService, _loginService, _ServicioService, _route, _router) {
+        this._TramiteEspecificoService = _TramiteEspecificoService;
+        this._VarianteService = _VarianteService;
+        this._CasoService = _CasoService;
+        this._VehiculoService = _VehiculoService;
+        this._loginService = _loginService;
+        this._ServicioService = _ServicioService;
+        this._route = _route;
+        this._router = _router;
+        this.servicioSeleccionado = null;
+        this.varianteTramite = null;
+        this.casoTramite = null;
+        this.tramiteGeneralId = 22;
+        this.vehiculo = null;
+        this.tramiteCreado = new core_1.EventEmitter();
+        this.datos = {
+            'nuevo': null,
+            'viejo': null
+        };
+    }
+    NewTramiteCambioServicioComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        var token = this._loginService.getToken();
+        this._CasoService.showCasosTramite(token, 5).subscribe(function (response) {
+            _this.casos = response.data;
+        }, function (error) {
+            _this.errorMessage = error;
+            if (_this.errorMessage != null) {
+                console.log(_this.errorMessage);
+                alert("Error en la petición");
+            }
+        });
+        this._VarianteService.showVariantesTramite(token, 5).subscribe(function (response) {
+            _this.variantes = response.data;
+        }, function (error) {
+            _this.errorMessage = error;
+            if (_this.errorMessage != null) {
+                console.log(_this.errorMessage);
+                alert("Error en la petición");
+            }
+        });
+        this._ServicioService.getServicio().subscribe(function (response) {
+            _this.servicios = response.data;
+        }, function (error) {
+            _this.errorMessage = error;
+            if (_this.errorMessage != null) {
+                console.log(_this.errorMessage);
+                alert("Error en la petición");
+            }
+        });
+        this.tramiteEspecifico = new TramiteEspecifico_1.TramiteEspecifico(null, 6, this.tramiteGeneralId, null, null, null);
+    };
+    NewTramiteCambioServicioComponent.prototype.onChangeServicio = function (event) {
+        for (var i = 0; i < this.servicios.length; ++i) {
+            if (event == this.servicios[i].id) {
+                this.servicioSeleccionado = this.servicios[i];
+                this.datos.nuevo = this.servicioSeleccionado.nombre;
+            }
+        }
+    };
+    NewTramiteCambioServicioComponent.prototype.enviarTramite = function () {
+        var _this = this;
+        var token = this._loginService.getToken();
+        this._TramiteEspecificoService.register2(this.tramiteEspecifico, token, this.datos).subscribe(function (response) {
+            _this.respuesta = response;
+            if (_this.respuesta.status == "success") {
+                _this.tramiteCreado.emit(true);
+            }
+            (function (error) {
+                _this.errorMessage = error;
+                if (_this.errorMessage != null) {
+                    console.log(_this.errorMessage);
+                    alert("Error en la petición");
+                }
+            });
+        });
+        this.vehiculo2 = new vehiculo_1.Vehiculo(this.vehiculo.id, this.vehiculo.clase.id, this.vehiculo.municipio.id, this.vehiculo.linea.id, this.servicioSeleccionado.id, this.vehiculo.color.id, this.vehiculo.combustible.id, this.vehiculo.carroceria.id, this.vehiculo.organismoTransito.id, this.vehiculo.placa, this.vehiculo.numeroFactura, this.vehiculo.fechaFactura, this.vehiculo.valor, this.vehiculo.numeroManifiesto, this.vehiculo.fechaManifiesto, this.vehiculo.cilindraje, this.vehiculo.modelo, this.vehiculo.motor, this.vehiculo.chasis, this.vehiculo.serie, this.vehiculo.vin, this.vehiculo.numeroPasajeros);
+        this._VehiculoService.editVehiculo(this.vehiculo2, token).subscribe(function (response) {
+            _this.respuesta = response;
+            (function (error) {
+                _this.errorMessage = error;
+                if (_this.errorMessage != null) {
+                    console.log(_this.errorMessage);
+                    alert("Error en la petición");
+                }
+            });
+        });
+    };
+    NewTramiteCambioServicioComponent.prototype.onChangeCaso = function (event) {
+        this.tramiteEspecifico.casoId = event;
+    };
+    NewTramiteCambioServicioComponent.prototype.onChangeVariante = function (event) {
+        this.tramiteEspecifico.varianteId = event;
+    };
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Object)
+    ], NewTramiteCambioServicioComponent.prototype, "tramiteGeneralId", void 0);
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Object)
+    ], NewTramiteCambioServicioComponent.prototype, "vehiculo", void 0);
+    __decorate([
+        core_1.Output(), 
+        __metadata('design:type', Object)
+    ], NewTramiteCambioServicioComponent.prototype, "tramiteCreado", void 0);
+    NewTramiteCambioServicioComponent = __decorate([
+        core_1.Component({
+            selector: 'tramiteCambioServicio',
+            templateUrl: 'app/view/tipoTramite/cambioServicio/index.html',
+            directives: [router_1.ROUTER_DIRECTIVES],
+            providers: [login_service_1.LoginService, servicio_service_1.ServicioService, tramiteEspecifico_service_1.TramiteEspecificoService, vehiculo_service_1.VehiculoService, variante_service_1.VarianteService, caso_service_1.CasoService]
+        }), 
+        __metadata('design:paramtypes', [tramiteEspecifico_service_1.TramiteEspecificoService, variante_service_1.VarianteService, caso_service_1.CasoService, vehiculo_service_1.VehiculoService, login_service_1.LoginService, servicio_service_1.ServicioService, router_1.ActivatedRoute, router_1.Router])
+    ], NewTramiteCambioServicioComponent);
+    return NewTramiteCambioServicioComponent;
+}());
+exports.NewTramiteCambioServicioComponent = NewTramiteCambioServicioComponent;
+//# sourceMappingURL=index.cambioServicio.component.js.map
