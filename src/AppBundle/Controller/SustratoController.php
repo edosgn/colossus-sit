1af7b7b69f97d50229a5e9ddbaa4b63d72ea5b68
Edgar Guerrero <edgargn87@gmail.com>
2018-04-17 08:19:02 -0500

    Correccion sustrato

--- src/AppBundle/Controller/SustratoController.php
+++ src/AppBundle/Controller/SustratoController.php
@@ -7,8 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Request;
-
 /**
  * Sustrato controller.
  *
