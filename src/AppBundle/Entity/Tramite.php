dca952bd67c428542e784f43a8f2e818b5db2433
Edgar Guerrero <edgargn87@gmail.com>
2018-08-01 05:53:32 -0500

    Actualizacion Tramite entity

--- src/AppBundle/Entity/Tramite.php
+++ src/AppBundle/Entity/Tramite.php
@@ -57,13 +57,6 @@ class Tramite
     private $sustrato = false;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="formulario", type="string", length=255, nullable=true)
-     */
-    private $formulario;
-
-    /**
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Modulo", inversedBy="Tramites")
      **/
     protected $modulo;
@@ -221,29 +214,5 @@ class Tramite
     public function getModulo()
     {
         return $this->modulo;
-    }
-
-    /**
-     * Set formulario
-     *
-     * @param string $formulario
-     *
-     * @return Tramite
-     */
-    public function setFormulario($formulario)
-    {
-        $this->formulario = $formulario;
-
-        return $this;
-    }
-
-    /**
-     * Get formulario
-     *
-     * @return string
-     */
-    public function getFormulario()
-    {
-        return $this->formulario;
     }
 }
