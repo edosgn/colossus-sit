5017b767514751e2ebfea336207e61f9bb42f663
JhoanCarlosama <jhoan.carlosama@gmail.com>
2019-03-07 17:59:04 -0500

    Sync

---
+++ src/JHWEB/UsuarioBundle/Entity/UserEmpresaRepresentante.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace JHWEB\UsuarioBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UserEmpresaRepresentante
+ *
+ * @ORM\Table(name="user_empresa_representante")
+ * @ORM\Entity(repositoryClass="JHWEB\UsuarioBundle\Repository\UserEmpresaRepresentanteRepository")
+ */
+class UserEmpresaRepresentante
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fechaInicial", type="date")
+     */
+    private $fechaInicial;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fechaFinal", type="date", nullable=true)
+     */
+    private $fechaFinal;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="activo", type="boolean")
+     */
+    private $activo = true;
+
+
+    /** @ORM\ManyToOne(targetEntity="JHWEB\UsuarioBundle\Entity\UserEmpresa", inversedBy="representanteEmpresa") */
+    private $empresa;
+
+    /** @ORM\ManyToOne(targetEntity="JHWEB\UsuarioBundle\Entity\UserCiudadano", inversedBy="representanteEmpresa") */
+    private $ciudadano;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fechaInicial
+     *
+     * @param \DateTime $fechaInicial
+     *
+     * @return UserEmpresaRepresentante
+     */
+    public function setFechaInicial($fechaInicial)
+    {
+        $this->fechaInicial = $fechaInicial;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaInicial
+     *
+     * @return \DateTime
+     */
+    public function getFechaInicial()
+    {
+        return $this->fechaInicial;
+    }
+
+    /**
+     * Set fechaFinal
+     *
+     * @param \DateTime $fechaFinal
+     *
+     * @return UserEmpresaRepresentante
+     */
+    public function setFechaFinal($fechaFinal)
+    {
+        $this->fechaFinal = $fechaFinal;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaFinal
+     *
+     * @return \DateTime
+     */
+    public function getFechaFinal()
+    {
+        return $this->fechaFinal;
+    }
+
+    /**
+     * Set activo
+     *
+     * @param boolean $activo
+     *
+     * @return UserEmpresaRepresentante
+     */
+    public function setActivo($activo)
+    {
+        $this->activo = $activo;
+
+        return $this;
+    }
+
+    /**
+     * Get activo
+     *
+     * @return boolean
+     */
+    public function getActivo()
+    {
+        return $this->activo;
+    }
+
+    /**
+     * Set empresa
+     *
+     * @param \JHWEB\UsuarioBundle\Entity\UserEmpresa $empresa
+     *
+     * @return UserEmpresaRepresentante
+     */
+    public function setEmpresa(\JHWEB\UsuarioBundle\Entity\UserEmpresa $empresa = null)
+    {
+        $this->empresa = $empresa;
+
+        return $this;
+    }
+
+    /**
+     * Get empresa
+     *
+     * @return \JHWEB\UsuarioBundle\Entity\UserEmpresa
+     */
+    public function getEmpresa()
+    {
+        return $this->empresa;
+    }
+
+    /**
+     * Set ciudadano
+     *
+     * @param \JHWEB\UsuarioBundle\Entity\UserCiudadano $ciudadano
+     *
+     * @return UserEmpresaRepresentante
+     */
+    public function setCiudadano(\JHWEB\UsuarioBundle\Entity\UserCiudadano $ciudadano = null)
+    {
+        $this->ciudadano = $ciudadano;
+
+        return $this;
+    }
+
+    /**
+     * Get ciudadano
+     *
+     * @return \JHWEB\UsuarioBundle\Entity\UserCiudadano
+     */
+    public function getCiudadano()
+    {
+        return $this->ciudadano;
+    }
+}
