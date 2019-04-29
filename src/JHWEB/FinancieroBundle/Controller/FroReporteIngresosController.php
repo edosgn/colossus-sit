3bb8e84d9961b7a5955932e08058a3f877f1673c
JhoanCarlosama <jhoan.carlosama@gmail.com>
2019-04-29 17:56:18 -0500

    Sync

---
+++ src/JHWEB/FinancieroBundle/Controller/FroReporteIngresosController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace JHWEB\FinancieroBundle\Controller;
+
+use JHWEB\FinancieroBundle\Entity\FroReporteIngresos;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Froreporteingreso controller.
+ *
+ * @Route("froreporteingresos")
+ */
+class FroReporteIngresosController extends Controller
+{
+    /**
+     * Lists all froReporteIngreso entities.
+     *
+     * @Route("/", name="froreporteingresos_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $froReporteIngresos = $em->getRepository('JHWEBFinancieroBundle:FroReporteIngresos')->findAll();
+
+        return $this->render('froreporteingresos/index.html.twig', array(
+            'froReporteIngresos' => $froReporteIngresos,
+        ));
+    }
+
+    /**
+     * Finds and displays a froReporteIngreso entity.
+     *
+     * @Route("/{id}", name="froreporteingresos_show")
+     * @Method("GET")
+     */
+    public function showAction(FroReporteIngresos $froReporteIngreso)
+    {
+
+        return $this->render('froreporteingresos/show.html.twig', array(
+            'froReporteIngreso' => $froReporteIngreso,
+        ));
+    }
+
+    /**
+     * datos para obtener tramites por rango de fechas
+     *
+     * @Route("/tramite/fecha", name="frotramite_rango_fechas")
+     * @Method({"GET", "POST"})
+     */
+    public function tramitesByFechaAction(Request $request)
+    {
+        $helpers = $this->get("app.helpers");
+        $hash = $request->get("authorization", null);
+        $authCheck = $helpers->authCheck($hash);
+        if ($authCheck == true) {
+            $json = $request->get("data", null);
+            $params = json_decode($json);
+            
+            $em = $this->getDoctrine()->getManager();
+            
+            $tramites = $em->getRepository('JHWEBFinancieroBundle:FroReporteIngresos')->findTramitesByFecha($params);
+
+            if($tramites){
+                $response = array(
+                    'status' => 'success',
+                    'code' => 200,
+                    'message' => "Trámites encontrados.",
+                    'data' => $tramites,
+                    'cant' => count($tramites),
+                );
+            } else {
+                $response = array(
+                    'status' => 'error',
+                    'code' => 400,
+                    'message' => "No se encontraron tramites para el organismo de tránsito con los rangos de fecha establecidos para la búsqueda.",
+                );
+            }
+        } else {
+            $response = array(
+                'status' => 'error',
+                'code' => 400,
+                'message' => "Autorizacion no válida",
+            );
+        }
+        return $helpers->json($response);
+    }
+}
