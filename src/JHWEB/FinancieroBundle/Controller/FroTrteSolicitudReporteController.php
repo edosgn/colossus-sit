55c76ec388562c93df85bcebc3f217eb58e26fe4
JhoanCarlosama <jhoan.carlosama@gmail.com>
2019-06-05 18:18:01 -0500

    Sync

--- src/JHWEB/FinancieroBundle/Controller/FroTrteSolicitudReporteController.php
+++ src/JHWEB/FinancieroBundle/Controller/FroTrteSolicitudReporteController.php
@@ -50,20 +50,57 @@ class FroTrteSolicitudReporteController extends Controller
             
             $tramitesSolicitud = null;
             $propietariosActuales = null;
-            
+            $tramites = null;
+            $medidasCautelares = null;
+            $cancelacionesMatricula = null;
+            $vehiculosPlaca = [];
             if($params->tipoReporte == 1) {
                 $tramitesSolicitud = $em->getRepository('JHWEBFinancieroBundle:FroTrteSolicitud')->getByPlaca($params->idOrganismoTransito, $params->idModulo, $fechaDesde, $fechaHasta);
             }
             else if($params->tipoReporte == 2){
                 $propietariosActuales = $em->getRepository('JHWEBFinancieroBundle:FroTrteSolicitud')->getPropietariosActualesByPlaca($params->idOrganismoTransito, $params->idModulo, $vehiculo->getId());
             }
-            if ($tramitesSolicitud || $propietariosActuales) {
+            else if($params->tipoReporte == 3) {
+                $tramites = $em->getRepository('JHWEBFinancieroBundle:FroTrteSolicitud')->getByTramites($params->idOrganismoTransito, $params->idModulo, $fechaDesde, $fechaHasta);
+            }
+            else if($params->tipoReporte == 4) {
+                $medidasCautelares = $em->getRepository('JHWEBFinancieroBundle:FroTrteSolicitud')->getByMedidasCautelares($fechaDesde, $fechaHasta);
+
+                foreach ($medidasCautelares as $key => $medidaCautelar) {
+                    $propietarios = $em->getRepository('JHWEBVehiculoBundle:VhloPropietario')->findBy(
+                        array(
+                            'ciudadano' => $medidaCautelar->getCiudadano()->getId(),
+                            'activo' => 1
+                        )
+                    );
+                }
+
+                foreach ($propietarios as $key => $propietario) {
+                    $vehiculosPlaca = array(
+                        'numero' => $propietario->getVehiculo()->getPlaca()->getNumero()
+                    );
+                }
+                /* if($vehiculosPlaca){
+                    $placasString = implode(',', '$vehiculosPlaca');
+                } */
+
+                /* $arrayMedidasCautelares = array(
+
+                ); */
+            }
+            else if($params->tipoReporte == 5) {
+                $cancelacionesMatricula = $em->getRepository('JHWEBFinancieroBundle:FroTrteSolicitud')->getByCancelacionMatricula($params->idOrganismoTransito, $params->idModulo, $fechaDesde, $fechaHasta);
+            }
+            if ($tramitesSolicitud || $propietariosActuales || $tramites || $medidasCautelares || $cancelacionesMatricula) {
                 $response = array(
                     'status' => 'success',
                     'code' => 200,
                     'message' => 'Registros encontrados.', 
                     'tramitesSolicitud'=> $tramitesSolicitud,
                     'propietariosActuales'=> $propietariosActuales,
+                    'tramites'=> $tramites,
+                    'medidasCautelares' => $medidasCautelares,
+                    'cancelacionesMatricula' => $cancelacionesMatricula
                 );
             }else{
                 $response = array(
