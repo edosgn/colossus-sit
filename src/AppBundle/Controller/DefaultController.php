4fdc2520b8018d0e6897a5596700d1b83ecd7f2d
Edgar Guerrero <edgargn87@gmail.com>
2019-07-15 13:09:14 -0500

    Sync

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,22 +18,22 @@ class DefaultController extends Controller
     /**
      * Creates a new Cuenta entity.
      *
-     * @Route("/pdf/certificadoTradicion/{placaId}/{tipo}", name="pdf")
+     * @Route("/pdf/certificadoTradicion/{placa}/{idFuncionario}/{tipo}", name="pdf")
      * @Method({"GET", "POST"})
      */
-    public function pdfAction(Request $request,$placaId,$tipo)
+    public function pdfAction(Request $request, $placa, $idFuncionario, $tipo)
     {
         setlocale(LC_ALL,"es_ES");
         $fechaActual = strftime("%d de %B del %Y");
 
         $em = $this->getDoctrine()->getManager();
         
-        /*$asignaciones = $em->getRepository('AppBundle:MpersonalAsignacion')->findByFuncionario(
-            1
-        ); */
+        $funcionario = $em->getRepository('JHWEBPersonalBundle:PnalFuncionario')->find(
+            $idFuncionario
+        );
         
         $vehiculo = $em->getRepository('JHWEBVehiculoBundle:VhloVehiculo')->findOneByPlaca(
-            $placaId 
+            $placa
         );
 
         if ($vehiculo) {
@@ -49,6 +49,7 @@ class DefaultController extends Controller
         $tramitesSolicitudArray = false;
         
         $data = null;
+
         foreach ($tramitesSolicitud as $tramiteSolicitud) {
 
             /*foreach ((array)$tramiteSolicitud->getResumen() as $key => $value) {
@@ -64,53 +65,12 @@ class DefaultController extends Controller
         
         $html = $this->renderView('@App/default/pdfCertificadoTradicion.html.twig', array(
             'vehiculo'=>$vehiculo,
+            'funcionario'=>$funcionario,
             'propietarios' => $propietarios,
             'fechaActual' => $fechaActual,
             'tramitesSolicitudArray'=>$tramitesSolicitudArray
         ));
 
         $this->get('app.pdf')->templateCertificadoTradicion($html, $vehiculo, 'oficial');
-
-        /*$pdf = $this->container->get("white_october.tcpdf")->create(
-            'PORTRAIT',
-            PDF_UNIT,
-            PDF_PAGE_FORMAT,
-            true,
-            'UTF-8',
-            false
-        );
-
-        $pdf->SetAuthor('qweqwe');
-        $pdf->SetTitle('Planilla');
-        $pdf->SetSubject('Your client');
-        $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
-        $pdf->setFontSubsetting(true);
-        $pdf->SetFont('helvetica', '', 11, '', true);
-        $pdf->SetMargins('25', '25', '25');
-        $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-        $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-        $pdf->AddPage();
-
-        if($tipo == 'OFICIAL'){
-            $image_file = __DIR__.'/../../../web/img/marcaAgua.png';
-            $pdf->Image($image_file, 50, 50, 100, '', '', '', '', false, 0);
-        }
- 
-        $pdf->writeHTMLCell(
-            $w = 0,
-            $h = 0,
-            $x = '',
-            $y = '',
-            $html,
-            $border = 0,
-            $ln = 1,
-            $fill = 0,
-            $reseth = true,
-            $align = '',
-            $autopadding = true
-        );
-
-        $pdf->Output("example.pdf", 'I');
-        die();*/
     }
 }
