cb9e2118fd00522501f4dafe577058e465bd64b5
Edgar Guerrero <edgargn87@gmail.com>
2019-07-23 15:48:37 -0500

    Sync

--- src/JHWEB/ConfigBundle/Controller/CfgPropietarioController.php
+++ src/JHWEB/ConfigBundle/Controller/CfgPropietarioController.php
@@ -138,7 +138,7 @@ class CfgPropietarioController extends Controller
     /**
      * Finds and displays a cfgPropietario entity.
      *
-     * @Route("/{id}", name="cfgpropietario_show")
+     * @Route("/{id}/show", name="cfgpropietario_show")
      * @Method("GET")
      */
     public function showAction(CfgPropietario $cfgPropietario)
@@ -154,26 +154,92 @@ class CfgPropietarioController extends Controller
     /**
      * Displays a form to edit an existing cfgPropietario entity.
      *
-     * @Route("/{id}/edit", name="cfgpropietario_edit")
+     * @Route("/edit", name="cfgpropietario_edit")
      * @Method({"GET", "POST"})
      */
-    public function editAction(Request $request, CfgPropietario $cfgPropietario)
+    public function editAction(Request $request)
     {
-        $deleteForm = $this->createDeleteForm($cfgPropietario);
-        $editForm = $this->createForm('JHWEB\ConfigBundle\Form\CfgPropietarioType', $cfgPropietario);
-        $editForm->handleRequest($request);
+        $helpers = $this->get("app.helpers");
+        $hash = $request->get("authorization", null);
+        $authCheck = $helpers->authCheck($hash);
 
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+        if ($authCheck== true) {
+            $json = $request->get("data",null);
+            $params = json_decode($json);
+           
+            $em = $this->getDoctrine()->getManager();
 
-            return $this->redirectToRoute('cfgpropietario_edit', array('id' => $cfgPropietario->getId()));
+            $propietario = $em->getRepository('JHWEBConfigBundle:CfgPropietario')->find(
+                $params->id
+            );
+
+
+            $propietario->setNombre(strtoupper($params->nombre));
+            $propietario->setNit($params->nit);
+            $propietario->setCorreo($params->correo);
+            $propietario->setTelefono($params->telefono);
+            $propietario->setCorreo($params->correo);
+            $propietario->setConceptos($params->conceptos);
+
+            //Carga de imagen de cabecera de página
+            if ($request->files->get('fileHeader')) {
+                $fileHeader = $request->files->get('fileHeader');
+                   
+                if ($fileHeader) {
+                    $extension = $fileHeader->guessExtension();
+                    $filename = "header.".$extension;
+                    $dir=__DIR__.'/../../../../web/img';
+    
+                    $fileHeader->move($dir,$filename);
+                    $propietario->setImagenCabecera($filename);
+                }
+            }
+
+            //Carga de imagen de pie de página
+            if ($request->files->get('fileFooter')) {
+                $fileFooter = $request->files->get('fileFooter');
+                   
+                if ($fileFooter) {
+                    $extension = $fileFooter->guessExtension();
+                    $filename = "footer.".$extension;
+                    $dir=__DIR__.'/../../../../web/img';
+    
+                    $fileFooter->move($dir,$filename);
+                    $propietario->setImagenPie($filename);
+                }
+            }
+
+            //Carga de imagen de logo de sistema
+            if ($request->files->get('fileLogo')) {
+                $fileLogo = $request->files->get('fileLogo');
+                   
+                if ($fileLogo) {
+                    $extension = $fileLogo->guessExtension();
+                    $filename = "logo.".$extension;
+                    $dir=__DIR__.'/../../../../web/img';
+    
+                    $fileLogo->move($dir,$filename);
+                    $propietario->setImagenLogo($filename);
+                }
+            }
+
+            $em->persist($propietario);
+            $em->flush();
+
+            $response = array(
+                'status' => 'success',
+                'code' => 200,
+                'message' => "Registro creado con exito",
+            );
+        }else{
+            $response = array(
+                'status' => 'error',
+                'code' => 400,
+                'message' => "Autorizacion no valida", 
+            );
         }
-
-        return $this->render('cfgpropietario/edit.html.twig', array(
-            'cfgPropietario' => $cfgPropietario,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
+        
+        return $helpers->json($response);
     }
 
     /**
