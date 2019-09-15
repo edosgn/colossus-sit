9a0a1df96ba1ca8eb8250097973a60e6fc0ee0ac
Edgar Guerrero <edgargn87@gmail.com>
2019-09-14 19:14:56 -0500

    Sync

--- src/JHWEB/FinancieroBundle/Controller/FroTrteSolicitudController.php
+++ src/JHWEB/FinancieroBundle/Controller/FroTrteSolicitudController.php
@@ -2658,20 +2658,36 @@ class FroTrteSolicitudController extends Controller
 
                 $dir=__DIR__.'/../../../../web/docs/';
                 $file = $dir."TTAMVEHI.DAT"; 
+
+                if( file_exists("datos.txt") == false ){
+                    $abrir = fopen($file,"r"); 
+                }else{
+                    $archivo = fopen($file, "w+b");    // Abrir el archivo, creándolo si no existe
+                }
  
-                $archivo = fopen($file, "w+b");    // Abrir el archivo, creándolo si no existe
-                if( $archivo == false ){
+                if($archivo == false){
                     echo("Error al crear el archivo");
                 }else{
-                    echo("El archivo ha sido creado");
+                    // Escribir en el archivo:
+                    fwrite($archivo, "Estamos probando\r\n");
+                    fwrite($archivo, "el uso de archivos ");
+                    fwrite($archivo, "en PHP");
+                    // Fuerza a que se escriban los datos pendientes en el buffer:
+                    fflush($archivo);
                 }
 
                 fclose($archivo);   // Cerrar el archivo
 
-                $response = new BinaryFileResponse($file);
-                $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT);
+                # Obtener nombre sin ruta completa, únicamente para sugerirlo al guardar
+                $nombreArchivo = basename($file);
+                # Algunos encabezados que son justamente los que fuerzan la descarga
+                header('Content-Type: application/octet-stream');
+                header("Content-Transfer-Encoding: Binary");
+                header("Content-disposition: attachment; filename=$nombreArchivo");
+                # Leer el archivo y sacarlo al navegador
+                readfile($file);
 
-                return $response;
+                return $file;
 
 
                 if ($vehiculos) {
