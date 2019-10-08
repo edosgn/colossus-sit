52a18a0f76d8f6a1cdd8a3dba538bdec7dabd02f
JhoanCarlosama <jhoan.carlosama@gmail.com>
2019-10-08 17:56:46 -0500

    Sync

--- src/AppBundle/services/PdfTemplate.php
+++ src/AppBundle/services/PdfTemplate.php
@@ -838,4 +838,59 @@ class PdfTemplate extends TCPDF{
         // This method has several options, check the source code documentation for more information.
         $pdf->Output('acta_asignacion_'.$asignacion->getNumeroActa().'.pdf', 'I');
     }
+
+    public function templateTarjetaControl($html, $titulo){
+        // create new PDF document
+        $pdf = new PdfTemplate('L', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+        // set document information
+        $pdf->SetCreator(PDF_CREATOR);
+        $pdf->SetAuthor('JHWEB PASTO SAS');
+        $pdf->SetTitle($titulo);
+        $pdf->SetSubject('TCPDF Tutorial');
+        $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+        // set default header data
+        $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
+
+        // set header and footer fonts
+        $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+        $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+        // set default monospaced font
+        $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+        // set margins
+        $pdf->SetMargins('15', '25', '15');
+        $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+        $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+        // set auto page breaks
+        $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+        // set image scale factor
+        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+        // set some language-dependent strings (optional)
+        if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+            require_once(dirname(__FILE__).'/lang/eng.php');
+            $pdf->setLanguageArray($l);
+        }
+
+        // ---------------------------------------------------------
+
+        // set font
+        $pdf->SetFont('helvetica', '', 8, '', true);
+
+        // Add a page
+        // This method has several options, check the source code documentation for more information.
+        $pdf->AddPage();
+
+        // Print text using writeHTMLCell()
+        $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+        // ---------------------------------------------------------
+        // Close and output PDF document
+        // This method has several options, check the source code documentation for more information.
+        $pdf->Output($titulo, 'I');
+    }
 }
